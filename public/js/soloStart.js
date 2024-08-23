b28bb455d3e4a018c1c8407aae06c58a5bd7de7e
kaito <4681kaito@gmail.com>
2024-08-23 23:19:40 +0900

    [update] Enterで効果音を鳴らしたあと、ゲーム開始

--- public/js/soloStart.js
+++ public/js/soloStart.js
@@ -1,33 +1,73 @@
-// ゲームスタート時に一回のみ実行する関数
-async function soloGameStart() {
-  try {
-    // POSTリクエストを送信し、レスポンスを受け取る
-    const response = await fetch(
-      '/solo/start',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ nextWord: nextWordInputText }),
-      },
-    );
-
-    // レスポンスをJSON形式に変換
-    const responseObj = await response.json();
-
-    // ステータスコードが200でなければエラーハンドリング
-    if (response.status !== 200) {
-      console.error('Error:', responseObj.message || 'Unknown error');
-    } else {
-      timer(responseObj.endtime);
-    }
-  } catch (error) {
-    // ネットワークエラーなどの例外をキャッチして処理
-    console.error('Fetch error:', error);
+// idを取得（cookie）
+async function getId() {
+	try {
+	  // GETリクエストを送信し、レスポンスを受け取る
+	  const response = await fetch('/getId', {
+		method: 'GET',
+		headers: {
+		  'Content-Type': 'application/json',
+		},
+	  });
+  
+	  // ステータスコードが200でなければエラーハンドリング
+	  if (response.status !== 200) {
+		const responseObj = await response.json();
+		console.error('Error:', responseObj.message || 'Unknown error');
+		return;
+	  }
+  
+	  // ステータスコードが200の場合、正常に処理されました
+	  console.log('ID retrieved successfully');
+	} catch (error) {
+	  // ネットワークエラーなどの例外をキャッチして処理
+	  console.error('Fetch error:', error);
+	}
   }
-}
-
-onload = (_event) => {
-  soloGameStart();
-};
+  
+  // ゲームスタート時に一回のみ実行する関数
+  async function soloGameStart() {
+	try {
+	  // POSTリクエストを送信し、レスポンスを受け取る
+	  const response = await fetch('/solo/start', {
+		method: 'GET',
+		headers: {
+		  'Content-Type': 'application/json',
+		},
+	  });
+  
+	  // ステータスコードが200でなければエラーハンドリング
+	  if (response.status !== 200) {
+		const responseObj = await response.json();
+		console.error('Error:', responseObj.message || 'Unknown error');
+		return;
+	  }
+  
+	  // 正常にレスポンスを受け取った場合、タイマーを開始
+	  const responseObj = await response.json();
+	  timer(responseObj.endtime);
+	} catch (error) {
+	  // ネットワークエラーなどの例外をキャッチして処理
+	  console.error('Fetch error:', error);
+	}
+  }
+  
+  // enterでゲームスタート
+  document.body.addEventListener('keydown', (event) => {
+	if (event.key === 'Enter') {
+	  const element = document.getElementById('enterToBegin');
+	  if (element) {
+		element.remove();
+		//   スタートのサウンドの再生
+		new Audio('sound/start.mp3').play();
+		console.log("play");
+		
+	  }
+	  soloGameStart(); // Enterキーが押された時にゲームを開始
+	}
+  }, { once: true });
+  
+  // ページロード時に実行する処理
+  window.onload = async (_event) => {
+	await getId();
+  };
+  
