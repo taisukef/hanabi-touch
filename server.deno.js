5955e901d6b19666053a2835b4d82d91481d6da0
tamagosushi <mitachi.ts75@gmail.com>
2024-08-23 14:25:40 +0900

    [add] id取得のapiを追加

--- server.deno.js
+++ server.deno.js
@@ -1,11 +1,26 @@
 import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
+import { setCookie, getCookies } from "https://deno.land/std@0.74.0/http/mod.ts";
+
+function makeId(){
+  return crypto.randomUUID();
+}
 
 Deno.serve(async (req) => {
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
 
   if (req.method === "GET" && pathname === "/welcome-message") {
-    return new Response("jigインターンへようこそ！");
+    Response("jigインターンへようこそ！");
+  }
+  if (req.method === "GET" && pathname === "/getId"){
+    return new Response(
+      "set id", {
+        status: 200,
+        headers: {
+          "set-cookie": `id=${makeId()}`
+        }
+      }
+    );
   }
 
   return serveDir(req, {
