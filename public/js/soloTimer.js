65c1cb3e63cc31754b1c429e6a94f1378fb4371e
y2210700 <y2210700@gl.cc.uec.ac.jp>
2024-08-24 00:05:40 +0900

    [update] timerを呼び出した瞬間に残り時間を更新するように変更

--- public/js/soloTimer.js
+++ public/js/soloTimer.js
@@ -1,16 +1,18 @@
-function updateTimer(gameTime) {
+function updateTimer(remainTime) {
   const timerId = document.getElementById('timer');
-  timerId.textContent = `残り${gameTime}秒`;
+  timerId.textContent = `残り${remainTime}秒`;
 }
 
-function timer(endTime) {
-  let remainTime = (endTime - new Date().getTime()) / 1000;
+function timer(endTime) { //endTime: サーバーから送られるゲームの終了時刻が入る
+  /*一応手元だとこの記述で10秒～0秒まで自然にカウントダウンする
+   サーバー側から終了時刻が送られる→以下の記述が発動するまでの計算時間によっては中途半端な時間からカウントダウンするかも*/
+  updateTimer(Math.round((endTime - new Date().getTime()) / 1000));
 
   const intervalId = setInterval(() => {
+    const remainTime = Math.round((endTime - new Date().getTime()) / 1000);
     updateTimer(remainTime);
 
-    if (remainTime < 0) {
-		localStorage.clear();
+    if (remainTime <= 0) {
       clearInterval(intervalId);
       location.href = './soloResult.html';
     }
