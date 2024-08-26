4aa29a48371e36865be5a40ad18adb0ca46b8a33
kaito <4681kaito@gmail.com>
2024-08-26 11:39:24 +0900

    Revert "Merge branch 'main' into add-solo-game"
    This reverts commit a009d7724bcdd6e6d82a5d55725b66d0d20daced, reversing
    changes made to 75430081e7c5147ece854204185262e1d31df888.

--- server.deno.js
+++ server.deno.js
@@ -1,27 +1,11 @@
 import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
-import { getCookies } from "https://deno.land/std@0.74.0/http/mod.ts";
-
-function makeId(){
-  return crypto.randomUUID();
-}
 
 Deno.serve(async (req) => {
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
 
   if (req.method === "GET" && pathname === "/welcome-message") {
-    Response("jigインターンへようこそ！");
-  }
-  if (req.method === "GET" && pathname === "/getId"){
-    if(getCookies(req)["id"]) return new Response("id is already set");
-    return new Response(
-      "set id", {
-        status: 200,
-        headers: {
-          "set-cookie": `id=${makeId()}`
-        }
-      }
-    );
+    return new Response("jigインターンへようこそ！");
   }
 
   return serveDir(req, {
