c7c3ecac7b0d715517e6b4125dd62490b8f6f047
y2210700 <y2210700@gl.cc.uec.ac.jp>
2024-08-26 17:49:50 +0900

    [update] 花火が打ちあがらなくなったので記述する場所を変更

--- public/js/fireworkCanvas.js
+++ public/js/fireworkCanvas.js
@@ -38,12 +38,12 @@ function draw() {
 
   colorMode(HSB);
   // 花火の更新
-  graphicBuffers[0].background(bgcolor);
-  graphicBuffers[1].background(bgcolor);
-  graphicBuffers[2].background(bgcolor);
   graphicBuffers[0].background(0, Math.ceil(255 / raisingTrail));
   graphicBuffers[1].background(0, Math.ceil(255 / botanTrail));
   graphicBuffers[2].background(0, Math.ceil(255 / kikuTrail));
+  graphicBuffers[0].background(bgcolor);
+  graphicBuffers[1].background(bgcolor);
+  graphicBuffers[2].background(bgcolor);
 
   const delta = deltaTime;
   const currentFrame = frameRate();
