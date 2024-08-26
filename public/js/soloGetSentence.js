7ea6d9c77571b868e50d27819dbbd82fb0ccd13a
kaito <4681kaito@gmail.com>
2024-08-26 23:17:24 +0900

    [fix] コメントの修正と、関数名の変更

--- public/js/soloGetSentence.js
+++ public/js/soloGetSentence.js
@@ -2,7 +2,7 @@
  * 文章、想定入力時間を取得し、入力する文字に設定する。
  * meterの初期化もする。
  */
-async function getSentence() {
+async function fetchSentenceAndRefreshMeter() {
   let response;
 
   try {
@@ -16,24 +16,21 @@ async function getSentence() {
 
     const responseObj = await response.json();
 
-	
     // ステータスコードが200でなければエラーハンドリング
     if (response.status !== 200) {
-		console.error('Error:', responseObj.message || 'Unknown error');
-		location.reload();
-		return;
-    }else{
-		// もらった変数で設定するように変更する
-		setMeter(responseObj.expectedTime);
-		startMeter();
-	
-		//   文章を設定
-		document.getElementById('notEntered').textContent =
-		  responseObj.sentenceAlphabet;
-		document.getElementById('japanese').textContent =
-		  responseObj.sentenceJapanese;
-		
-	}
+      console.error('Error:', responseObj.message || 'Unknown error');
+      location.reload();
+      return;
+    }
+    // メーターを設定
+    setMeter(responseObj.expectedTime);
+    startMeter();
+
+    //  文章を設定
+    document.getElementById('notEntered').textContent =
+      responseObj.sentenceAlphabet;
+    document.getElementById('japanese').textContent =
+      responseObj.sentenceJapanese;
   } catch (error) {
     // ネットワークエラーなどの例外をキャッチして処理
     console.error('Fetch error:', error);
