9c26c1ce6fd857e6af4fa508afe21ec3aedbf7c8
y2210700 <y2210700@gl.cc.uec.ac.jp>
2024-08-26 14:23:43 +0900

    [update] deno lint で指摘されたところを修正

--- public/js/soloResult.js
+++ public/js/soloResult.js
@@ -7,35 +7,35 @@
   typeCount: 何回タイプできたか
 */
 
-window.onload = async (event) => {
-    // サーバーから結果情報を受け取る
-    const response = await fetch('/solo/getResult', { method: 'GET' });
-    const responseJson = await response.text();
-    const responseObj = JSON.parse(responseJson);
+onload = async (event) => {
+  // サーバーから結果情報を受け取る
+  const response = await fetch('/solo/getResult', { method: 'GET' });
+  const responseJson = await response.text();
+  const responseObj = JSON.parse(responseJson);
 
-    //スコアの表示
-    const scoreResult = document.querySelector('#score');
-    scoreResult.innerHTML = responseObj['score'];
+  //スコアの表示
+  const scoreResult = document.querySelector('#score');
+  scoreResult.innerHTML = responseObj['score'];
 
-    //花火を打ち上げた回数を表示
-    const fireworkCountResult = document.querySelector('#fireworkCount');
-    fireworkCountResult.innerHTML = responseObj['fireworkCount'];
+  //花火を打ち上げた回数を表示
+  const fireworkCountResult = document.querySelector('#fireworkCount');
+  fireworkCountResult.innerHTML = responseObj['fireworkCount'];
 
-    //単位時間当たりのタイプ回数の表示
-    const typesPerSecondResult = document.querySelector('#typesPerSecond');
-    typesPerSecondResult.innerHTML = responseObj['typesPerSecond'];
+  //単位時間当たりのタイプ回数の表示
+  const typesPerSecondResult = document.querySelector('#typesPerSecond');
+  typesPerSecondResult.innerHTML = responseObj['typesPerSecond'];
 
-    //タイプした回数の表示
-    const typeCountResult = document.querySelector('#typeCount');
-    typeCountResult.innerHTML = responseObj['typeCount'];
+  //タイプした回数の表示
+  const typeCountResult = document.querySelector('#typeCount');
+  typeCountResult.innerHTML = responseObj['typeCount'];
 };
 
 // タイトルに戻るボタン。ひとまず、ボタンのidが「titleButton」と仮定して作ってます。
-document.querySelector('#titleButton').onclick = async (event) => {
-    location.href = './index.html';
+document.querySelector('#titleButton').onclick = (event) => {
+  location.href = './index.html';
 };
 
 // もう一度遊ぶボタン。現段階では、ボタンのidが「restartButton」と仮定して作っています。
-document.querySelector('#restartButton').onclick = async (event) => {
-    location.href = './solo.html';
+document.querySelector('#restartButton').onclick = (event) => {
+  location.href = './solo.html';
 };
