f78dcc31a4e077e76986c6d4e339f18454a86675
y2210700 <y2210700@gl.cc.uec.ac.jp>
2024-08-27 15:04:22 +0900

    [update] 小数点以下を四捨五入から切り捨てにしました

--- public/js/soloTimer.js
+++ public/js/soloTimer.js
@@ -6,10 +6,10 @@ function updateTimer(remainTime) { // 残り時間の記述の更新
 function timer(endTime) { //endTime: サーバーから送られるゲームの終了時刻が入る
   /*一応手元だとこの記述で10秒～0秒まで自然にカウントダウンする。0秒時にページ遷移することも確認しました
    サーバー側から終了時刻が送られる→以下の記述が発動するまでの計算時間によっては中途半端な時間からカウントダウンするかも*/
-  updateTimer(Math.round((endTime - new Date().getTime()) / 100) / 10);
+  updateTimer(Math.floor((endTime - new Date().getTime()) / 100) / 10);
 
   const intervalId = setInterval(() => { // 1秒ごとに以下の処理を行う
-    const remainTime = Math.round((endTime - new Date().getTime()) / 100) / 10;
+    const remainTime = Math.floor((endTime - new Date().getTime()) / 100) / 10;
     updateTimer(remainTime);
 
     if (remainTime <= 0) {
