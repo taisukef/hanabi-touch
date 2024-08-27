9ab94ff0b9c8178b9ccfda08dc37c6912c2492f9
kaito <4681kaito@gmail.com>
2024-08-27 16:03:33 +0900

    [fix] scoreの設定方法を修正

--- public/js/soloScore.js
+++ public/js/soloScore.js
@@ -1,16 +1,15 @@
 /** 指定された値に、得点を初期化して、表示 */
 function initializeScore(initializedScore){
-	const score = document.getElementById('score');
-	score.textContent = initializedScore;
+	updateScore(initializedScore)
 	const scorePanel = document.getElementById('scorePanel');
-	scorePanel.style.display = block;
+	scorePanel.style.display = "block";
 }
 
 /**
  * 得点を追加する関数
- * @param {number} addScore 追加する特典 
+ * @param {number} score 設定する得点
  */
-function updateScore(addScore){
-	const score = document.getElementById('score');
-	score.textContent = parseInt(score.textContent) + addScore;
+function updateScore(score){
+	const scoreElem = document.getElementById('score');
+	scoreElem.textContent = score;
 }
