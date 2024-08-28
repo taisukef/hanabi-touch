6bfdeecb16982fb24f9bbaf467e8dc00dbefd7b3
y2210700 <y2210700@gl.cc.uec.ac.jp>
2024-08-28 22:00:21 +0900

    [update] 誤字の修正

--- public/js/ranking.js
+++ public/js/ranking.js
@@ -6,15 +6,15 @@ onload = async (event) => {
     const response = await fetch('/solo/getRanking', { method: 'GET' });
     const responseJson = await response.text();
     const responseObj = JSON.parse(responseJson);
-    const ranking = responseObj['top10Results'];
 
-    for (let i = 0; i < ranking.length; i++) {
-        const rank = document.querySelector(`#rank${i + 1}`);
-        const name = document.querySelector(`#name${i + 1}`);
-        const score = document.querySelector(`#score${i + 1}`);
-        rank.innerHTML = `${i + 1}位`;
-        name.innerHTML = `${ranking[i + 1]['userName']}`;
-        score.innerHTMK = `${ranking[i + 1]['score']}`;
+    for (let i = 0; i < responseObj['top10Results'].length; i++) {
+        const ranking = responseObj['top10Results'][i];
+        const rankId = document.querySelector(`#rank${i + 1}`);
+        const nameId = document.querySelector(`#name${i + 1}`);
+        const scoreId = document.querySelector(`#score${i + 1}`);
+        rankId.innerHTML = `${i + 1}位`;
+        nameId.innerHTML = `${ranking['userName']}`;
+        scoreId.innerHTML = `${ranking['score']}`;
     }
-    //console.log(responseObj['top10Results'][0]['score']);
+    console.log(responseObj['top10Results'][0]);
 };
