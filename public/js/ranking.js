bda60535cb2dcdf7310997eda385ed99be1a7e58
y2210700 <y2210700@gl.cc.uec.ac.jp>
2024-08-28 22:20:31 +0900

    [update] ランキングの取得が終わるまでボタンを見えなくするように変更

--- public/js/ranking.js
+++ public/js/ranking.js
@@ -6,15 +6,18 @@ onload = async (event) => {
     const response = await fetch('/solo/getRanking', { method: 'GET' });
     const responseJson = await response.text();
     const responseObj = JSON.parse(responseJson);
+    const ranking = responseObj['top10Results'];
 
-    for (let i = 0; i < responseObj['top10Results'].length; i++) {
-        const ranking = responseObj['top10Results'][i];
+    for (let i = 0; i < ranking.length; i++) {
         const rankId = document.querySelector(`#rank${i + 1}`);
         const nameId = document.querySelector(`#name${i + 1}`);
         const scoreId = document.querySelector(`#score${i + 1}`);
         rankId.innerHTML = `${i + 1}位`;
-        nameId.innerHTML = `${ranking['userName']}`;
-        scoreId.innerHTML = `${ranking['score']}`;
+        nameId.innerHTML = `${ranking[i]['userName']}`;
+        scoreId.innerHTML = `${ranking[i]['score']}`;
     }
     console.log(responseObj['top10Results'][0]);
+
+    document.querySelector('#top10').style.visibility = 'visible';
+    document.querySelector('#titleButton').style.visibility = 'visible';
 };
