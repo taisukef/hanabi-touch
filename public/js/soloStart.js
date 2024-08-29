cbd8771be4681afc07fad6cbc255c5f439ca1a5d
kaito <4681kaito@gmail.com>
2024-08-29 11:32:03 +0900

    [fix] option画面を変更

--- public/js/soloStart.js
+++ public/js/soloStart.js
@@ -71,51 +71,6 @@ async function soloGameStart(difficulty) {
   }
 }
 
-// enter　その時選択している難易度でゲームスタート
-// ← →  難易度を変更
-document.addEventListener('keydown', async (event) => {
-  const option = document.getElementById('option');
-  if (!option) return;
-
-  const element = option.querySelector('.selected');
-  if (!element) return;
-  if (event.key === 'Enter') {
-    soloGameStart(element.value);
-    option.remove();
-    start.play();
-  } else if (event.key === 'ArrowLeft') {
-    if (element.value === 'easy') return;
-    element.classList.remove('selected');
-
-    if (element.value === 'normal') {
-      option.querySelector('[value="easy"]').classList.add('selected');
-    } else {
-      option.querySelector('[value="normal"]').classList.add('selected');
-    }
-  } else if (event.key === 'ArrowRight') {
-    if (element.value === 'hard') return;
-    element.classList.remove('selected');
-
-    if (element.value === 'normal') {
-      option.querySelector('[value="hard"]').classList.add('selected');
-    } else {
-      option.querySelector('[value="normal"]').classList.add('selected');
-    }
-  }
-});
-
-// 難易度選択画面でボタンをクリックして始める
-document.querySelectorAll('.difficultyButton').forEach((button) => {
-  button.addEventListener('click', () => {
-    const option = document.getElementById('option');
-    if (option) {
-      soloGameStart(button.value);
-      option.remove();
-      start.play();
-    }
-  });
-});
-
 // ページロード時に実行する処理
 onload = async (_event) => {
   await getId();
